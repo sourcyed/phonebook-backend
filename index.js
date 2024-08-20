d8d2e3da408b6cde3c4b417abd1b1c672fcd2d9b
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-08-20 13:46:45 +0300

    Added the frontend dist

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors())
 app.use(express.json())
 morgan.token('body', function (req, res) { JSON.stringify(req.body); return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let persons = [
     { 
