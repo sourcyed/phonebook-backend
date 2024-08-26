72498bb39a81b5f35f675f46084da69b03769edb
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-08-26 16:12:49 +0300

    Finished ex3.15, ex3.16, ex3.17 & ex3.18

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const cors = require('cors')
 const Person = require('./models/person')
 const app = express()
 
-app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(cors())
 morgan.token('body', function (req, res) { JSON.stringify(req.body); return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(express.static('dist'))
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(pp => response.json(pp))
@@ -19,16 +19,31 @@ app.get('/info', (request, response) => {
         ${new Date()}`))
 })
 
-app.get('/api/persons:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-    Person.findById(id).then(p => p ? response.json(p) : response.status(404).end)
+    Person.findById(id)
+        .then(p => p ? response.json(p) : response.status(404).end)
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
     Person.findByIdAndDelete(id).then(result =>
         response.status(204).end()
-    ).catch(result => response.status(404).end())
+    ).catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => response.json(updatedPerson))
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request,response) => {
@@ -53,6 +68,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
